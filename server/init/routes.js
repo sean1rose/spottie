75508c926dbc07ab5fdf3d8d93819dd3eca3a230
Sean Rose <sean1rose@gmail.com>
2017-01-17 01:51:56 -0800

    clean up routes.js

--- server/init/routes.js
+++ server/init/routes.js
@@ -4,9 +4,6 @@
 import passport from 'passport';
 import unsupportedMessage from '../db/unsupportedMessage';
 import { controllers, passport as passportConfig } from '../db';
-// import cheerio from 'cheerio';
-// import request from 'request';
-// import fs from 'fs';
 
 const usersController = controllers && controllers.users;
 const topicsController = controllers && controllers.topics;
@@ -14,59 +11,7 @@ const scrapeController = controllers && controllers.scrape;
 
 export default (app) => {
   app.get('/scrape', scrapeController.scrape);
-  // app.get('/scrape', (req, res) => {
-    // // scrape here...
-    // var finalUrl = 'http://thehuddle.com/stats/2016/plays_weekly.php?week=1&pos=qb&col=FPTS&ccs=1';
-		// var options = {
-		// 	url: finalUrl,
-		// 	headers: {
-		// 		'Origin': 'http://thehuddle.com'
-		// 	}
-		// };
-    // var finalObj = {};
 
-    // request(options, (err, res, html) => {
-    //   if (!err) {
-    //     var $ = cheerio.load(html);
-    //     var arrayOfPlayers = [];
-    //     var objectOfPlayers = {};
-
-    //     $('div.mod-table > table > tbody > tr').each(function( index ) {
-    //       var player = $(this).find('td.t_std_left.align-left > a').text().trim();
-          
-    //       var children = $(this).children();
-    //       var rank = parseInt(index) + 1;
-    //       var playerName = children.find('a').text().trim();
-    //       var team = children.eq(1).text().trim();
-    //       var targets = parseInt(children.eq(7).text().trim());
-    //       var catches = parseInt(children.eq(8).text().trim());
-    //       var fantasyPts = parseInt(children.eq(3).text().trim());
-    //       var pointsPerTarget = (fantasyPts / targets);
-          
-    //       var playerObj = {};
-    //       playerObj['playerName'] = playerName;
-    //       playerObj['rank'] = rank;
-    //       playerObj['team'] = team;
-    //       playerObj['targets'] = targets;
-    //       playerObj['catches'] = catches;
-    //       playerObj['fantasyPoints'] = fantasyPts;
-    //       playerObj['pointsPerTarget'] = pointsPerTarget;
-
-    //       objectOfPlayers[playerName] = playerObj;
-
-    //       arrayOfPlayers.push(playerObj);
-
-    //       finalObj['objectOfPlayers'] = objectOfPlayers;
-    //       finalObj['arrayOfPlayers'] = arrayOfPlayers;
-    //     });
-    //     // console.log('finalObj - ', finalObj);
-    //   }
-    // });
-    // fs.writeFile('output.json', JSON.stringify(finalObj, null, 4), (err) => {
-    //   console.log('CHECK FOR OUTPUT.JSON!!!')
-    // });
-  // });
-  
   // user routes
   if (usersController) {
     app.post('/login', usersController.login);
